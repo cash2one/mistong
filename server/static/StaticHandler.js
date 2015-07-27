51c6d13e6963c2c6d621741de1e695594805d52f
fedora <fedora1989@gmail.com>
2015-07-27 12:13:57 +0800

    修改文件静态文件访问路径,添加了新的common模块

--- server/static/StaticHandler.js
+++ server/static/StaticHandler.js
@@ -5,16 +5,31 @@ var path = require("path"),
     
 var log4js = require('log4js');
 var logger = log4js.getLogger();
-
+var staticpath={
+    "other":"other",
+    "src":'src',
+    "dist":'dist',
+    "common":'common'
+}
 
 module.exports = function(req, res, cab) {
 
     var urlarg = url.parse(req.url);
+    var pripath=urlarg.path.split("/");
+
+    var realPath ="";
+    if(staticpath[pripath[1]]){
+        //获取真实的路径
+        realPath = urlarg.pathname.substr(1);
+    }else{
+        realPath = "dist" +urlarg.pathname;
+    }
+
+    logger.info("realpath:"+__dirname);
     var ext = path.extname(urlarg.pathname);
     ext = ext ? ext.slice(1) : 'unknown';
-
     var contentType = mime[ext] || "text/plain";
-    var realPath = "other" + urlarg.pathname;
+    
     fs.exists(realPath, function(exists) {
         if (exists) {
             fs.readFile(realPath, "binary", function(err, file) {
